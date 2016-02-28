2c229d9a76aad4b317527d824dadb8f2adea01c9
LiinaMarcela <lmbarrera35@misena.edu.co>
2016-02-28 10:42:20 -0500

    Operaciones

---
+++ Operaciones/src/operaciones/Operaciones.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+/**
+ *
+ * @author klf
+ */
+public class Operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo ");
+        int a = 5, b = 10;
+        int r = a + b;
+        System.out.println(r);
+        int c = 5, d = 10;
+        int r1 = c - d;
+        System.out.println(r1);
+
+        int e = 5, f = 10;
+        int r2 = e / f;
+        System.out.println(r2);
+
+        int g = 5, h = 10;
+        int r3 = g / h;
+        System.out.println(r3);
+
+    }
+}
